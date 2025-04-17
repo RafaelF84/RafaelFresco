0643c6630d078cdf8aa5356ff93af927a25f8f8d
RafaelF84 <rafaasfrescoo@gmail.com>
2025-04-17 19:50:39 +0100

    Add files via upload

---
+++ scypt.js
@@ -0,0 +1,9 @@
+// Navegação suave para rolar até as seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
